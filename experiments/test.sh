27d973cfc2732e5fe1e6c67c233ddf56d22797f5
Syrinx <syrinx_w@163.com>
2025-06-14 19:07:31 +0100

    test.sh

---
+++ experiments/test.sh
@@ -0,0 +1,20 @@
+python3 STICI_V1.1.py \
+    --mode train \
+
+# python3 STICI_V1.1.py \
+#     --mode train \
+#     --which-chunk 1 \
+#     --save-dir ./alaki \
+#     --ref ./data/STI_benchmark_datasets/ALL.chr22.training.samples.100k.any.type.0.01.maf.variants.vcf.gz \
+#     --min-mr 0.85 \
+#     --max-mr 0.95 \
+#     --cs 2048 \
+#     --sites-per-model 10240 \
+#     --co 64 \
+#     --na-heads 16 \
+#     --embed-dim 128 \
+#     --batch-size-per-gpu 4 \
+#     --tihp 1 \
+#     --lr 0.002 \ 
+#     --restart-training 1 \
+#     --verbose 1 \
