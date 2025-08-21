f344fce6dd2fa0cf0580d62cc239e5d802a66bd7
Syrinx <syrinx_w@163.com>
2025-08-21 21:56:00 +0100

    sh wandb runname

--- experiments/test.sh
+++ experiments/test.sh
@@ -8,7 +8,7 @@ python3 STICI_V1.1.py \
     --epochs 10 \
     --batch-size-per-gpu 3 \
     --use-wandb true \
-    --wandb-runname "test_wandb_each loss" \
+    --wandb-runname "test_lossvalue" \
 
 # python3 STICI_V1.1.py \
 #     --mode train \
