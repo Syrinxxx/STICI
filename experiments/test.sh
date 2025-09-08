b0a020b409b8d7c82b60bf84b037d578fe719f33
Syrinx <syrinx_w@163.com>
2025-09-08 04:18:03 +0100

    sh 10epoch base

--- experiments/test.sh
+++ experiments/test.sh
@@ -5,10 +5,10 @@ python3 STICI_V1.1.py \
     --tihp 1 \
     --save-dir ./alaki \
     --use-r2 true \
-    --epochs 50 \
+    --epochs 10 \
     --batch-size-per-gpu 3 \
     --use-wandb true \
-    --wandb-runname "try_base50_allchunk" \
+    --wandb-runname "try_base10_allchunk" \
 
 # python3 STICI_V1.1.py \
 #     --mode impute \
