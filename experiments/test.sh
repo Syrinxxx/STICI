d3ec8c8b4415d8e5f0bc6b5f2d55daa9bc21f3f2
Syrinx <syrinx_w@163.com>
2025-09-12 22:38:47 +0100

    test HPC_Impute

--- experiments/test.sh
+++ experiments/test.sh
@@ -1,14 +1,26 @@
 python3 STICI_V1.1.py \
-    --mode train \
-    --restart-training 1 \
+    --mode impute \
+    --target ./data/hapgen/simu3_con_30720.vcf \
     --ref ./data/hapgen/22merged_30720.vcf.gz \
     --tihp 1 \
     --save-dir ./alaki \
     --use-r2 true \
-    --epochs 50 \
+    --epochs 2 \
     --batch-size-per-gpu 3 \
     --use-wandb true \
-    --wandb-runname "try_base50_allchunk_2" \
+    --wandb-runname "testHPC_Impute" \
+
+# python3 STICI_V1.1.py \
+#     --mode train \
+#     --restart-training 1 \
+#     --ref ./data/hapgen/22merged_30720.vcf.gz \
+#     --tihp 1 \
+#     --save-dir ./alaki \
+#     --use-r2 true \
+#     --epochs 50 \
+#     --batch-size-per-gpu 3 \
+#     --use-wandb true \
+#     --wandb-runname "try_base50_allchunk_2" \
 
 # python3 STICI_V1.1.py \
 #     --mode impute \
